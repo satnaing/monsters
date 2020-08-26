055d9ae213784462de7f1ad489557f5f449ee2a3
Sat Naing <satnaingdev@gmail.com>
2020-08-26 14:32:45 +0630

    Updates

---
+++ precache-manifest.3da47f4f168b2790f16b9caaaa9aa66c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d37415b40c723ab383f283472dad90",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "ecad9fa5268e253736c3",
+    "url": "/monsters/static/css/main.e20cf908.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecad9fa5268e253736c3",
+    "url": "/monsters/static/js/main.ee7407a3.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
